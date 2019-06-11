3c7ad945d5a9dc5e6c5062944717fccf9744117b
Alex Gascón <alexgascon.93@gmail.com>
2019-06-11 23:57:04 +0200

    Changing blog title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Alex Gascon`,
     author: `Alex Gascon`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
